f62a672696420a8cdccc31a1b9ab3c387e59c244
Siddharth Roy <siddharthroy36912@gmail.com>
2023-05-28 11:37:42 +0530

    Fixed social links

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -53,8 +53,7 @@ export const Header: React.FC<Props> = ({ project }) => {
       >
         <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
           <div className="flex justify-between gap-8">
-            
-            <Link target="_blank" href="https://twitter.com/chronark_">
+            <Link target="_blank" href="https://twitter.com/reactoverflow">
               <Twitter
                 className={`w-6 h-6 duration-200 hover:font-medium ${
                   isIntersecting
@@ -63,7 +62,7 @@ export const Header: React.FC<Props> = ({ project }) => {
                 } `}
               />
             </Link>
-            <Link target="_blank" href="https://github.com/chronark">
+            <Link target="_blank" href="https://github.com/siddharthroy12">
               <Github
                 className={`w-6 h-6 duration-200 hover:font-medium ${
                   isIntersecting
